f96776785d879f93a4ec3d3f96b8dc5b764aeb67
wapdeveloper <xspangrui@outlook.com>
2017-09-14 11:35:50 +0800

    1
    1

---
+++ 地图操作-分割shp/code/MapOperation/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapOperation
+{
+    public class Grid
+    {
+        public List<Columns> Rows;
+        public List<object> Filed;
+    }
+    public class Columns
+    {
+        public List<object> columns;
+    }
+}
